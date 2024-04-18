4d77c127bcb486992c6fa77942c87aeb6301af1b
Toojeronix <manajeronimo7@gmail.com>
2024-04-18 02:25:45 -0300

    Modified Index Back

--- back/index.js
+++ back/index.js
@@ -1,9 +1,7 @@
 
 const app = require("./src/server")
 
-const PORT = 3000; // Puerto en el que escuchar
-const HOST = '192.168.100.71'; // Dirección IP en la que escuchar
-
+const PORT = 3000; 
 
 app.listen(PORT, () => {
     console.log("Serividor listo en el 3000");
